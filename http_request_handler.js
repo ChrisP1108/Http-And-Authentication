4a69d4bba2918b70f224381fc57999005799b1c7
Chris Paschall <ChrisP1108@gmail.com>
2023-08-14 19:19:19 -0700

    All Requests Merged As Init
    Requests can now be made by passing in an array of objects with "url", "method", "headers", and "data" keys.

--- http_request_handler.js
+++ http_request_handler.js
@@ -36,163 +36,58 @@ class HttpReq {
         return response;
     }
 
-    // Arguments Parser
+    // Request Error Handler
 
-    static #argsParser(args) {
-        const output = [];
-        args.forEach(item => {
-            if (typeof item === "object" && item.length) {
-                item.forEach(i => output.push(i))
-            } else output.push(item);
-        });
-        return output;
-    }
-
-    // Arguments Error Handler
-
-    static #argError(arg, dataNeeded, method) {
-        const statusKeys = { ok: false, status: null }
-        if (!arg || !arg.url) {
-            const noParamMsg = `No parameter data provided for ${method} request.`;
+    static #reqError(req, dataNeeded) {
+        const statusKeys = { ok: false, status: null };
+        if (!req) {
+            const noParamMsg = `No parameter data provided for http request. Parameter must have an object with a "method" and "url" key. For "POST" and "PUT" requests, a "data" must also be provided.`;
             console.error(noParamMsg);
             return { ...statusKeys, msg: noParamMsg };
-        } else if (dataNeeded && !arg.data) {
-            const noDataParam = `${method} request must have an object parameter with "url" and "data" keys`;
+        } else if (!req.method) {
+            const noMethodMsg = `No http request method parameter provided request.  Parameter must have an object with a "method" key.`;
+            console.error(noMethodMsg);
+            return { ...statusKeys, msg: noMethodMsg };
+        } else if (!req.url) {
+            const noUrlMsg = `No http request url parameter data provided for ${req.method} request.  Parameter must have an object with a "url" key.`;
+            console.error(noUrlMsg);
+            return { ...statusKeys, msg: noUrlMsg };
+        } else if (dataNeeded && !req.data) {
+            const noDataParam = `${req.method} request must have an object parameter with a "data" key.`;
             console.error(noDataParam);
             return { ...statusKeys, msg: noDataParam };
         } else return false;
     }
 
-    static async get() {
-        const args = this.#argsParser([...arguments]);
-        const output = [];
-        const method = "GET";
-        for (let i = 0; args.length > i; i++) {
-            let response;
-            if (typeof args[i] === "string") {
-                const str = args[i];
-                args[i] = {};
-                args[i].url = str;
-            }
-            response = this.#argError(args[i], false, method);
-            if (!response) {
-                const headers = this.#headers(args[i].headers);
-                try {
-                    const res = await fetch(args[i].url, {
-                        method,
-                        headers
-                    });
-                    if (res.ok) {
-                        response = await this.#successRequest(res, headers);
-                    } else {
-                        response = this.#statusError(res, args[i], method, headers);
-                    }
-                } catch (err) {
-                    response = this.#fetchFailed(err, headers);
-                }
-                response.request_data = null;
-                response.url = args[i].url;
-            }
-            response.method = method;
-            output.push(response);
-        }
-        return output;
-    }
+    // HTTP Request Handler
 
-    static async post() {
-        const args = this.#argsParser([...arguments]);
+    static async init(reqs) {
         const output = [];
-        const method = "POST";
-        for (let i = 0; args.length > i; i++) {
+        for (let i = 0; reqs.length > i; i++) {
             let response;
-            response = this.#argError(args[i], true, method);
+            const getOrDeleteReq = reqs[i].method.toLowerCase() === "get" || reqs[i].method.toLowerCase() === "delete";
+            response = this.#reqError(reqs[i], getOrDeleteReq === false);
             if (!response) {
-                const headers = this.#headers(args[i].headers);
+                const method = reqs[i].method.toUpperCase();
+                const headers = this.#headers(reqs[i].headers);
+                const reqParams = { method, headers };
+                if (getOrDeleteReq === false) {
+                    reqParams.body = JSON.stringify(reqs[i].data);
+                }
                 try {
-                    const res = await fetch(args[i].url, {
-                        method,
-                        headers,
-                        body: JSON.stringify(args[i].data)
-                    });
+                    const res = await fetch(reqs[i].url, reqParams);
                     if (res.ok) {
                         response = await this.#successRequest(res, headers);
                     } else {
-                        response = this.#statusError(res, args[i].url, method, headers);
+                        response = this.#statusError(res, reqs.url, method, headers);
                     }
                 } catch (err) {
                     response = this.#fetchFailed(err, headers);
                 }
-                response.request_data = args[i].data;
-                response.url = args[i].url;
+                response.request_data = reqs[i].data;
+                response.url = reqs[i].url;
             }
-            response.method = method;
-            output.push(response);
-        }
-        return output;
-    }
-
-    static async put() {
-        const args = this.#argsParser([...arguments]);
-        const output = [];
-        const method = "PUT";
-        for (let i = 0; args.length > i; i++) {
-            let response;
-            response = this.#argError(args[i], true, method);
-            if (!response) {
-                try {
-                    const headers = this.#headers(args[i].headers);
-                    const res = await fetch(args[i].url, {
-                        method,
-                        headers,
-                        body: JSON.stringify(args[i].data)
-                    });
-                    if (res.ok) {
-                        response = await this.#successRequest(res, headers);
-                    } else {
-                        response = this.#statusError(res, args[i].url, method, headers);
-                    }
-                } catch (err) {
-                    response = this.#fetchFailed(err, headers);
-                }
-                response.request_data = args[i].data;
-                response.url = args[i].url;
-            }
-            response.method = method;
-            output.push(response);
-        }
-        return output;
-    }
-
-    static async delete() {
-        const args = this.#argsParser([...arguments]);
-        const output = [];
-        const method = "DELETE";
-        for (let i = 0; args.length > i; i++) {
-            let response;
-            if (typeof args[i] === "string") {
-                const str = args[i];
-                args[i] = {};
-                args[i].url = str;
-            }
-            response = this.#argError(args[i], false, method);
-            if (!response) {
-                try {
-                    const headers = this.#headers(args[i].headers);
-                    const res = await fetch(args[i].url, {
-                        method,
-                        headers
-                    });
-                    if (res.ok) {
-                        response = await this.#successRequest(res, headers);
-                    } else {
-                        response = this.#statusError(res, args[i], method, headers);
-                    }
-                } catch (err) {
-                    response = this.#fetchFailed(err, headers);
-                }
-                response.url = args[i];
-            }
-            response.method = method;
+            response.method = reqs[i].method;
             output.push(response);
         }
         return output;
